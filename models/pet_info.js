948e55ce7830d214cbd4a40b83b04689b1b79c6b
Luke Chambers <lukechambers91@gmail.com>
2017-08-19 10:19:01 -0400

    first pet commit

---
+++ models/pet_info.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+    var Pet = sequelize.define("Pet", {
+      pet_name: DataTypes.STRING,
+      pet_birthday: DataTypes.DATETIME,
+      pet_species: DataTypes.STRING,
+      pet_breed: DataTypes.STRING,
+      pet_about: DataTypes.STRING,
+    });
+  
+    Pet.associate = function(models) {
+      // We're saying that a Pet should belong to an User
+      // A Pet can't be created without an User due to the foreign key constraint
+      Pet.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Pet;
+  };
+  
---
+++ models/user.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      birthday: DataTypes.DATETIME,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      about: DataTypes.STRING,
+    },{
+        paranoid: true
+    });
+  
+    User.associate = function(models) {
+      // Associating User with Posts
+      // When an User is deleted, also delete any associated Posts
+      User.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
+  };
