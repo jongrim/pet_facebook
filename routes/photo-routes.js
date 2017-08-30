7061546e91950c4d53a6d15ec9c1d9f0a7d126f9
Jonathan Grim <jonjongrim@gmail.com>
2017-08-30 20:04:40 +0100

    formatting

--- routes/photo-routes.js
+++ routes/photo-routes.js
@@ -9,82 +9,97 @@ const Pet = require('../models').Pet;
 
 // tells multer how to store images
 const storage = multer.diskStorage({
-    destination: './public/images', // this is where we want to save images
-    filename: (req, file, cb) => {
-        // the callback function returns the file name which is
-        // currently just the current date plus the original
-        // extension. Multer doesn't set extensions!
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+  destination: './public/images', // this is where we want to save images
+  filename: (req, file, cb) => {
+    // the callback function returns the file name which is
+    // currently just the current date plus the original
+    // extension. Multer doesn't set extensions!
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
 });
-
 
 // make an upload function to use in the route
 const upload = multer({ storage: storage });
 
-module.exports = function (app, passport) {
-    app.get('/photos', function (req, res) {
-        if (req.isAuthenticated()) {
-            User.findAll({
-                where: {
-                  id: req.user.dataValues.id,
-                },
-                include: [Pet, Photo]
-              }).then(function (data) {
-                res.render('photos', { user: req.user, Photo: data[0].Photos, Pet: data[0].Pets });
-              });
-        } else {
-            res.render('index', { message: req.flash('loginMessage')[0] });
-        }
-    });
+module.exports = function(app, passport) {
+  app.get('/photos', function(req, res) {
+    if (req.isAuthenticated()) {
+      User.findAll({
+        where: {
+          id: req.user.dataValues.id
+        },
+        include: [Pet, Photo]
+      }).then(function(data) {
+        res.render('photos', {
+          user: req.user,
+          Photo: data[0].Photos,
+          Pet: data[0].Pets
+        });
+      });
+    } else {
+      res.render('index', { message: req.flash('loginMessage')[0] });
+    }
+  });
 
-    //!!!!Resubmitting the page post the image to cloudinary again!!!!!!
-    //a pet must be created before you add any photos
-    app.post('/photos', upload.single('userFile'), function (req, res, next) {
-        if (req.isAuthenticated()) {
-            cloudinary.uploader.upload(req.file.path, result => {
-                Photo.create({
-                    isPet: true,
-                    likes: 1,
-                    img_url: result.secure_url,
-                    PetId: req.body.pet,
-                    UserId: req.user.dataValues.id
-                })
-                .then(
-                    fs.unlink(req.file.path, function (error) {
-                        if (error) {
-                            throw error;
-                        }
-                        console.log('Deleted ' + req.file.path.replace(/^.*[\\\/]/, '') + " from server.");
-                        res.redirect('profile');
-                    }));
-            });
-        } else {
-            res.render('index', { message: req.flash('loginMessage')[0] });
-        }
-    })
+  //!!!!Resubmitting the page post the image to cloudinary again!!!!!!
+  //a pet must be created before you add any photos
+  app.post('/photos', upload.single('userFile'), function(req, res, next) {
+    if (req.isAuthenticated()) {
+      cloudinary.uploader.upload(req.file.path, result => {
+        Photo.create({
+          isPet: true,
+          likes: 1,
+          img_url: result.secure_url,
+          PetId: req.body.pet,
+          UserId: req.user.dataValues.id
+        }).then(
+          fs.unlink(req.file.path, function(error) {
+            if (error) {
+              throw error;
+            }
+            console.log(
+              'Deleted ' +
+                req.file.path.replace(/^.*[\\\/]/, '') +
+                ' from server.'
+            );
+            res.redirect('profile');
+          })
+        );
+      });
+    } else {
+      res.render('index', { message: req.flash('loginMessage')[0] });
+    }
+  });
 
-    app.post('/profilePhotos', upload.single('userFile'), function (req, res, next) {
-        if (req.isAuthenticated()) {
-            cloudinary.uploader.upload(req.file.path, result => {
-                Photo.create({
-                    isPet: false,
-                    likes: 1,
-                    img_url: result.secure_url,
-                    PetId: null,
-                    UserId: req.user.dataValues.id
-                })
-                .then(
-                    fs.unlink(req.file.path, function (error) {
-                        if (error) {
-                            throw error;
-                        }
-                        console.log('Deleted ' + req.file.path.replace(/^.*[\\\/]/, '') + " from server.");
-                        res.redirect('profile');
-                    }));
-            });
-        } else {
-            res.render('index', { message: req.flash('loginMessage')[0] });
-        }
-    })
+  app.post('/profilePhotos', upload.single('userFile'), function(
+    req,
+    res,
+    next
+  ) {
+    if (req.isAuthenticated()) {
+      cloudinary.uploader.upload(req.file.path, result => {
+        Photo.create({
+          isPet: false,
+          likes: 1,
+          img_url: result.secure_url,
+          PetId: null,
+          UserId: req.user.dataValues.id
+        }).then(
+          fs.unlink(req.file.path, function(error) {
+            if (error) {
+              throw error;
+            }
+            console.log(
+              'Deleted ' +
+                req.file.path.replace(/^.*[\\\/]/, '') +
+                ' from server.'
+            );
+            res.redirect('profile');
+          })
+        );
+      });
+    } else {
+      res.render('index', { message: req.flash('loginMessage')[0] });
+    }
+  });
 };
